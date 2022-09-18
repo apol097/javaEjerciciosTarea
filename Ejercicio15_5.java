b4b0b3471fb34e221df0714290008e89c6729474
apolo <danielrivera076783@gmail.com>
2022-09-18 11:35:31 -0600

    Mapa 5 ejercicio 15 agregado

---
+++ Ejercicio15_5.java
@@ -0,0 +1,18 @@
+public class Ejercicio15_5 {
+    public static void main(String[] args) {
+      int _columnas, _filas;
+      _columnas = _filas = 5;
+      
+      for(int filas = 1 ; filas <= _filas ; filas++){
+        for( int columnas = 1 ; columnas <= _columnas ; columnas++){
+            if(filas%2 == 0){
+                System.out.print(" *");
+            }else {
+                System.out.print("* "); 
+            }
+        }
+        System.out.println("");
+      }
+    }
+}
+
